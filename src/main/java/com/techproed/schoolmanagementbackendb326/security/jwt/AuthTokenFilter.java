f22b524335d39d3c9b8627448a2630e1798b9e7d
levenmartinov <levkomartinov@gmail.com>
2025-02-14 23:56:40 +0000

    Project Day3.2

--- src/main/java/com/techproed/schoolmanagementbackendb326/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/security/jwt/AuthTokenFilter.java
@@ -2,24 +2,31 @@ package com.techproed.schoolmanagementbackendb326.security.jwt;
 
 
 import com.techproed.schoolmanagementbackendb326.security.service.UserDetailServiceImpl;
+
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 @RequiredArgsConstructor
+// for extra information please read
+//https://www.baeldung.com/spring-onceperrequestfilter
 public class AuthTokenFilter extends OncePerRequestFilter {
 
-    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthTokenFilter.class);
 
     @Autowired
     private JwtUtils jwtUtils;
@@ -35,15 +42,22 @@ public class AuthTokenFilter extends OncePerRequestFilter {
             //1-from every request, we will get JWT
             String jwt = parseJwt(request);
             //validate JWT
-            if(jwt != null && jwtUtils.validateToken(jwt)) {
+            if (jwt != null && jwtUtils.validateToken(jwt)) {
                 //3- we need username to get user information
                 String username = jwtUtils.getUsernameFromToken(jwt);
                 //4- check DB and fetch user and upgrade it to userDetails
                 UserDetails userDetails = userDetailService.loadUserByUsername(username);
                 //5- set attribute with username
                 request.setAttribute("username", username);
+                //6- we load user details information to security context
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
+        } catch (UsernameNotFoundException e) {
+            LOGGER.error("Can not set user authentication", e);
         }
+        filterChain.doFilter(request, response);
     }
 
     //Authorization -> Bearer ljsdfnkltskdfnvszlkfnvaqqdfknvaefkdsnvsacdfjknvcaldknsvcal
