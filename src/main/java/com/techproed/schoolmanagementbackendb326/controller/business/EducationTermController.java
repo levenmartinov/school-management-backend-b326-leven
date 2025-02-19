d25294fd6aed9462857c0e2b86244353652e281a
levenmartinov <levkomartinov@gmail.com>
2025-02-19 18:29:35 +0000

    Education Term

--- src/main/java/com/techproed/schoolmanagementbackendb326/controller/business/EducationTermController.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/controller/business/EducationTermController.java
@@ -3,15 +3,24 @@ package com.techproed.schoolmanagementbackendb326.controller.business;
 import com.techproed.schoolmanagementbackendb326.payload.request.business.EducationTermRequest;
 import com.techproed.schoolmanagementbackendb326.payload.response.business.EducationTermResponse;
 import com.techproed.schoolmanagementbackendb326.payload.response.business.ResponseMessage;
-import com.techproed.schoolmanagementbackendb326.service.business.EducationTermService;
+import com.techproed.schoolmanagementbackendb326.service.businnes.EducationTermService;
+
+
+import java.util.List;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/educationTerm")
@@ -20,11 +29,60 @@ public class EducationTermController {
 
     private final EducationTermService educationTermService;
 
-    @PreAuthorize("hasAnyAuthority('Admin', 'Dean')")
+    @PreAuthorize("hasAnyAuthority('Admin','Dean')")
     @PostMapping("/save")
-    public ResponseMessage<EducationTermResponse> save (
+    public ResponseMessage<EducationTermResponse>save(
             @Valid @RequestBody EducationTermRequest educationTermRequest) {
         return educationTermService.save(educationTermRequest);
     }
+
+    @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher')")
+    @PutMapping("/update/{educationTermId}")
+    public ResponseMessage<EducationTermResponse>updateEducationTerm(
+            @Valid @RequestBody EducationTermRequest educationTermRequest,
+            @PathVariable Long educationTermId){
+        return educationTermService.updateEducationTerm(educationTermRequest,educationTermId);
+    }
+
+
+    //TODO
+    // ummu
+    @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher')")
+    @GetMapping("/getAll")
+    public List<EducationTermResponse>getAllEducationTerms(){
+        //return educationTermService.getAllEducationTerms();
+        return null;
+    }
+
+
+    //TODO
+    // esra
+    @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher')")
+    @GetMapping("/{educationTermId}")
+    public EducationTermResponse getEducationTerm(Long educationTermId) {
+        //return educationTermService.getEducationTermById(educationTermId);
+        return null;
+    }
+
+    @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher')")
+    @GetMapping("/getByPage")
+    public Page<EducationTermResponse>getByPage(
+            @RequestParam(value = "page",defaultValue = "0") int page,
+            @RequestParam (value = "size",defaultValue = "10") int size,
+            @RequestParam (value = "sort",defaultValue = "term") String sort,
+            @RequestParam (value = "type",defaultValue = "desc") String type) {
+        return educationTermService.getByPage(page,size,sort,type);
+    }
+
+    @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher')")
+    @DeleteMapping("/delete/{educationTermId}")
+    public ResponseMessage deleteEducationTerm(@PathVariable Long educationTermId) {
+        return educationTermService.deleteById(educationTermId);
+    }
+
+
+
+
+
 
 }
